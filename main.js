8ed3e8bb37453ae897989812ebfd6d8f11963c00
Jeremy Tsukasa Zimmerman <jtzimmerm@gmail.com>
2019-03-06 18:21:17 -0500

    Side to side works, but up arrow only logs now

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
 let playerDiv = document.querySelector('.goldenTicket');
 
-window.addEventListener('keydown', moveLeftRight);
-function moveLeftRight(evt) {
+window.addEventListener('keydown', moveLeftUpRight);
+function moveLeftUpRight(evt) {
     let key_code = evt.which || evt.keyCode;
     switch (key_code) {
         case 37: //left arrow
+        console.log(key_code)
             charLeft();
             break;
+        case 38: //Up arrow
+        console.log(key_code)
+            charUp();
+            break;
         case 39: //right arrow
+        console.log(key_code)
             charRight();
             break;
     }
 }
 function charLeft() {
-    playerDiv.style.left = parseInt(playerDiv.style.left) - 15 + 'px';
+    playerDiv.style.left = parseInt(playerDiv.style.left) - 25 + 'px';
 }
 function charRight() {
-    playerDiv.style.left = parseInt(playerDiv.style.left) + 15 + 'px';
-    console.log("This variable is this:", playerDiv.style.left)
+    playerDiv.style.left = parseInt(playerDiv.style.left) + 25 + 'px';
 }
-
+function charUp() {
+    playerDiv.style.top = parseInt(playerDiv.style.top) - 25 + 'px';
+}
