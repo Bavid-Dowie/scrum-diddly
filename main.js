29e9323bbada0fa019f2785e2a7b6f0d214c60ad
Jeremy Tsukasa Zimmerman <jtzimmerm@gmail.com>
2019-03-07 11:41:55 -0500

    Arrow functions all working with a relative  understanding of why

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function moveLeftUpRight(evt) {
         console.log(key_code)
             charLeft();
             break;
-        case 38: //Up arrow
+        case 38: //up arrow
         console.log(key_code)
             charUp();
             break;
@@ -16,14 +16,26 @@ function moveLeftUpRight(evt) {
         console.log(key_code)
             charRight();
             break;
+        case 40: //down arrow
+        console.log(key_code)
+            charDown();
+            break;
     }
 }
 function charLeft() {
     playerDiv.style.left = parseInt(playerDiv.style.left) - 25 + 'px';
-}
-function charRight() {
-    playerDiv.style.left = parseInt(playerDiv.style.left) + 25 + 'px';
+    console.log("This variable is this:", playerDiv.style.left)
 }
 function charUp() {
     playerDiv.style.top = parseInt(playerDiv.style.top) - 25 + 'px';
+    console.log("This variable is this:", playerDiv.style.top)
 }
+function charRight() {
+    playerDiv.style.left = parseInt(playerDiv.style.left) + 25 + 'px';
+    console.log("This variable is this:", playerDiv.style.left)
+}
+// Optional move down:
+// function charDown() {
+//     playerDiv.style.top = parseInt(playerDiv.style.top) + 25 + 'px';
+//     console.log("This variable is this:", playerDiv.style.top)
+// }
