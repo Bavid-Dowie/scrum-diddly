442c3590fc150d79096196c0b220f41fa46364ea
Jeremy Tsukasa Zimmerman <jtzimmerm@gmail.com>
2019-03-08 11:08:33 -0500

    Score incrementing, ball reset at top, thanks tons Tara

--- main.js
+++ main.js
@@ -3,54 +3,26 @@ let scoreDiv = document.querySelector('.sidebarRightScore');
 let total = 0;
 
 let score = function () {
-    if (total < 20) {
+    if (total < 50) {
         total += 10
         scoreDiv.textContent = total
-        return total
+        console.log(total)
     } else {
         scoreDiv.textContent = total
-        console.log("Test: 1, 2, 3")
-        location.reload()
+        return total
     }
 }
-
-// const hitReaction = function () {
-//     score += 10;
-//     console.log(score)
-// }
-
-// aBall.addEventListener('click', hitReaction)
-
-// let scoreDiv = document.querySelector('.js-score')
-// let total = 0
-// let incrementScore = function () {
-  
-//     if (total >= 90) {
-//         document.querySelector('.level-winner').setAttribute('style', 'opacity: 1')
-   
-//     } else {
-//         total += 10
-//         scoreDiv.textContent = total
-
-//     }
-// }
-// aBall.addEventListener('click', incrementScore)
-
-
 window.addEventListener('keydown', moveLeftUpRight);
 function moveLeftUpRight(evt) {
     let key_code = evt.which || evt.keyCode;
     switch (key_code) {
         case 37: //left arrow
-            // console.log(key_code)
             charLeft();
             break;
         case 38: //up arrow
-            // console.log(key_code)
             charUp();
             break;
         case 39: //right arrow
-            // console.log(key_code)
             charRight();
             break;
     }
@@ -59,7 +31,7 @@ function charLeft() {
     playerDiv.style.left = parseInt(playerDiv.style.left) - 15 + 'px';
 }
 function charUp() {
-    playerDiv.style.top  = parseInt(playerDiv.style.top) - 15 + 'px';
+    playerDiv.style.top = parseInt(playerDiv.style.top) - 15 + 'px';
 }
 function charRight() {
     playerDiv.style.left = parseInt(playerDiv.style.left) + 15 + 'px';
@@ -71,21 +43,19 @@ function collisionHit() {
     const id = setInterval(frame, 0); // Rate of falling object
     function frame() {
         if (posit >= 750) {
-            clearInterval(id);
         } else {
             posit++;
             fall.style.top = posit + 'px';
-            // console.log(parseInt(fall.style.left) > parseInt(playerDiv.style.left) - 75)
             if (parseInt(playerDiv.style.top) < parseInt(fall.style.top)
                 && parseInt(fall.style.left) > parseInt(playerDiv.style.left)
                 && parseInt(fall.style.left) < parseInt(playerDiv.style.left) + 75) {
-                    console.log('Yum!')
-                    score()
-                }
-
-                // console.log('boom', playerDiv.style.top, fall.style.top)
-            // console.log(parseInt(fall.style.left), parseInt(playerDiv.style.left) - 75) 
-            // console.log(parseInt(playerDiv.width))
+                // console.log('Yum!')
+                    fall.style.top = 0;
+                    posit = 0;
+                // clearInterval(id);
+                console.log('test')
+                score()
+            }
         }
     }
 }
@@ -94,7 +64,8 @@ collisionHit()
 
 
 
-// let playerPosition = document.querySelector('#charlieBucket').getBoundingClientRect()
+
+                // let playerPosition = document.querySelector('#charlieBucket').getBoundingClientRect()
 // console.log(playerPosition)
 // let objectOne      = document.querySelector('#fallingObj').getBoundingClientRect()
 // console.log(objectOne)
